e96d5ffe092adf67a2d95b9ddc2de7d70d2ee586
Rob <amigo@quo.wtf>
2020-04-12 01:48:27 -0500

    feat: search and show data from scryfall api

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import {StyleSheet, SafeAreaView, Image, Text, Platform} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  Text,
+  ActivityIndicator,
+  Keyboard
+} from 'react-native';
 import {
   Button,
   Header,
@@ -9,48 +16,90 @@ import {
   Icon,
   Input,
   Title,
-  Body
+  Body,
+  View
 } from 'native-base';
+import {Cards, Card} from 'scryfall-sdk';
 
-class Search extends React.Component {
-  bgImage = require('../assets/landing.png');
-  render() {
-    return (
-      <SafeAreaView>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name="arrow-back" />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Search</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Header searchBar rounded style={styles.container}>
-          <Item>
-            <Icon name="search"></Icon>
-            <Input placeholder="Search"></Input>
-          </Item>
-        </Header>
-        <Button style={styles.button} block={true}>
-          <Text>back</Text>
-        </Button>
-      </SafeAreaView>
-    );
-  }
+import ListCards from './ListCards';
+
+const bgImage = require('../assets/landing.png');
+interface ViewStatus {
+  status: 'loading' | string;
 }
+
+const Search: React.FC = () => {
+  const [searchText, setSearchText] = useState<string>('');
+  const [viewStatus, setViewStatus] = useState<ViewStatus>({status: ''});
+  const [data, setData] = useState<Array<Card>>();
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Header>
+        <Left>
+          <Button transparent>
+            <Icon name="arrow-back" />
+          </Button>
+        </Left>
+        <Body>
+          <Title>Search</Title>
+        </Body>
+        <Right />
+      </Header>
+      <Header searchBar rounded style={styles.container}>
+        <Item>
+          <Icon name="search"></Icon>
+          <Input
+            placeholder="Search"
+            onChangeText={(text) => {
+              setSearchText(text);
+            }}></Input>
+        </Item>
+        <Button
+          bordered
+          small
+          onPress={() => {
+            setViewStatus({status: 'loading'});
+            Keyboard.dismiss;
+            Cards.query('/cards/search', {
+              q: searchText.toLowerCase() + ' lang:es'
+            })
+              .then((response: any) => {
+                setData(response.data);
+                setViewStatus({status: ''});
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }}>
+          {viewStatus.status === 'loading' ? (
+            <ActivityIndicator color="grey" />
+          ) : (
+            <Text>Go</Text>
+          )}
+        </Button>
+      </Header>
+      {data ? (
+        <View style={{flex: 1}}>
+          <ListCards data={data} searchText={searchText.toLowerCase()} />
+          <Button block large>
+            <Text>Add</Text>
+          </Button>
+        </View>
+      ) : (
+        <Image style={styles.bgCard} source={bgImage} />
+      )}
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
     borderBottomColor: '#fff'
   },
-  button: {
-    margin: 10
-  },
-  header: {}
+  bgCard: {
+    alignSelf: 'center',
+    height: '50%'
+  }
 });
 
 export default Search;
